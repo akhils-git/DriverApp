af60edd052b4114947ea154535fa5d37b7136d81
akhils-git <akhilmon@hotmail.com>
2025-10-12 19:21:15 +0530

    new page add user profile

--- app/src/main/java/com/girfalco/driverapp/ui/components/home_screen/HomeScreenUserInformationCard.kt
+++ app/src/main/java/com/girfalco/driverapp/ui/components/home_screen/HomeScreenUserInformationCard.kt
@@ -1,10 +1,14 @@
 package com.girfalco.driverapp.ui.components.home_screen
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import com.girfalco.driverapp.R
+import com.girfalco.driverapp.UserProfileActivity
 
 class HomeScreenUserInformationCard @JvmOverloads constructor(
     context: Context,
@@ -13,5 +17,14 @@ class HomeScreenUserInformationCard @JvmOverloads constructor(
 ) : RelativeLayout(context, attrs, defStyleAttr) {
     init {
         LayoutInflater.from(context).inflate(R.layout.homescreen_user_information_card, this, true)
+        
+        val profilePicture = findViewById<ImageView>(R.id.profile_picture)
+        profilePicture.setOnClickListener {
+            val intent = Intent(context, UserProfileActivity::class.java)
+            context.startActivity(intent)
+            if (context is Activity) {
+                context.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+        }
     }
 }
