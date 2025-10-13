fe6fcc753b8f4826ab90d93231006c621e26c41d
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2025-10-13 09:10:47 +0530

    edit button added on the user image

--- app/src/main/java/com/girfalco/driverapp/UserProfileActivity.kt
+++ app/src/main/java/com/girfalco/driverapp/UserProfileActivity.kt
@@ -7,5 +7,21 @@ class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        // Hide status bar and navigation bar for fullscreen
+        window.decorView.systemUiVisibility = (
+            android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+        )
+
+        // Set up back arrow click to finish activity
+        val backArrow = findViewById<android.widget.ImageView>(R.id.user_profile_back_arrow)
+        backArrow?.setOnClickListener {
+            finish()
+        }
     }
 }
